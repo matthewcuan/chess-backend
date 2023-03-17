384172d8bdece192dcade8437690f5caf9d7ef14
matthewcuan <cuanmatthewl@gmail.com>
2023-03-16 22:30:49 -0700

    attempting to deploy correctly

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ dotenv.config()
 const MongoClient = mongodb.MongoClient
 const chess_username = process.env.CHESS_ID
 const chess_password = process.env.CHESS_KEY
-const uri = `mongodb+srv://${chess_username}:${chess_password}@cluster0.mqza9mv.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://black:rules@cluster0.mqza9mv.mongodb.net/?retryWrites=true&w=majority`
 
-const port = 8000
+const port = process.env.PORT || 8000
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://melodious-speculoos-b36439.netlify.app/",
         methods: ["GET", "POST"],
         credentials: true,
     }
